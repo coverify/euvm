172fe3e1f15fc840d54865c4ba3683f934c03d16
Puneet Goel <puneet@coverify.com>
2016-04-02 11:00:11 +0530

    Added a c-model for SHA3

---
+++ examples/sha3/c-model/round.h
@@ -0,0 +1,4 @@
+#ifndef ROUND_H
+#define ROUND_H
+uint64_t **sha3_round(uint64_t **, uint64_t);
+#endif
---
+++ examples/sha3/c-model/sponge.h
@@ -0,0 +1,6 @@
+#ifndef SPONGE_H
+#define SPONGE_H
+uint8_t *sponge(uint8_t*,int32_t);
+uint8_t *padding(uint8_t*,int32_t*);
+void swap(char *, int);
+#endif
