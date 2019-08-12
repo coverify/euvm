720595daa1241f0d35aad9fae93fb8afa1fb4b1b
Puneet Goel <puneet@coverify.com>
2019-08-12 13:59:35 +0530

    purged some examples

--- examples/axi4sha3/c-model/round.h
+++
@@ -1,4 +0,0 @@
-#ifndef ROUND_H
-#define ROUND_H
-uint64_t **sha3_round(uint64_t **, uint64_t);
-#endif
--- examples/axi4sha3/c-model/sponge.h
+++
@@ -1,6 +0,0 @@
-#ifndef SPONGE_H
-#define SPONGE_H
-uint8_t *sponge(uint8_t*,int32_t);
-uint8_t *padding(uint8_t*,int32_t*);
-void swap(char *, int);
-#endif
